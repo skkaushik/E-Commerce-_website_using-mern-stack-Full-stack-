c3c29fdb078777fcdf68f53be5bcdbe44ada85ee
sachin <sksachinkaushik5362@gmail.com>
2023-07-31 23:57:02 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("This is backend")
+const express=require('express');
+const cors=require('cors');
+require('dotenv').config;
+const port=process.env.PORT||8080
+
+const app=express();
+app.use(cors());
+
+app.listen(port,()=>{
+    console.log(`This Server is Running On port no.${port}`)
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        This is E-Commerce Website .
+      </h1>
+    </div>
+  );
+}
+
+export default App;
