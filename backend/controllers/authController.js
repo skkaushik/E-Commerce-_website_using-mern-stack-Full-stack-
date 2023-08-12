736a617847cea432de6e9974887d59514e18bcbf
sachin <sksachinkaushik5362@gmail.com>
2023-08-12 19:10:13 +0530

    commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,14 @@
-
 import userModel from "../models/userModels.js";
-
 import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
+import orderModel from "../models/orderModel.js";
 
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address, answer } = req.body;
     //validations
     if (!name) {
-      return res.send({  message:"Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
       return res.send({ message: "Email is Required" });
@@ -66,7 +65,7 @@ export const registerController = async (req, res) => {
 export const loginController = async (req, res) => {
   try {
     const { email, password } = req.body;
-   
+
     //validation
     if (!email || !password) {
       return res.status(404).send({
@@ -103,7 +102,7 @@ export const loginController = async (req, res) => {
         email: user.email,
         phone: user.phone,
         address: user.address,
-        role:user.role,
+        role: user.role,
       },
       token,
     });
@@ -116,7 +115,6 @@ export const loginController = async (req, res) => {
     });
   }
 };
-
 
 export const forgotPasswordController = async (req, res) => {
   try {
@@ -155,7 +153,6 @@ export const forgotPasswordController = async (req, res) => {
   }
 };
 
-
 //test controller
 export const testController = (req, res) => {
   try {
@@ -165,7 +162,6 @@ export const testController = (req, res) => {
     res.send({ error });
   }
 };
-
 
 //update prfole
 export const updateProfileController = async (req, res) => {
@@ -197,6 +193,63 @@ export const updateProfileController = async (req, res) => {
     res.status(400).send({
       success: false,
       message: "Error WHile Update profile",
+      error,
+    });
+  }
+};
+
+//orders
+export const getOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ buyer: req.user._id })
+      .populate("products", "-photo")
+      .populate("buyer", "name");
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error WHile Geting Orders",
+      error,
+    });
+  }
+};
+//orders
+export const getAllOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({})
+      .populate("products", "-photo")
+      .populate("buyer", "name")
+      .sort({ createdAt: "-1" });
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Geting Orders",
+      error,
+    });
+  }
+};
+
+//order status
+export const orderStatusController = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const orders = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Updateing Order",
       error,
     });
   }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { registerController ,loginController, testController, forgotPasswordController, updateProfileController} from '../controllers/authController.js';
+import { registerController ,loginController, testController, forgotPasswordController, updateProfileController, orderStatusController, getAllOrdersController, getOrdersController} from '../controllers/authController.js';
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 
@@ -35,5 +35,19 @@ router.get("/admin-auth", requireSignIn,isAdmin, (req, res) => {
 
 //update profile
 router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
 
 export default router;
