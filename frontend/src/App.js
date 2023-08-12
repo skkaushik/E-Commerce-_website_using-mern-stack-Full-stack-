cc1aa48849e83e8bf71a5a443bf1474a53abfc28
sachin <sksachinkaushik5362@gmail.com>
2023-08-12 16:26:41 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,8 @@ import Products from "./pages/admin/Products";
 import UpdateProduct from "./pages/admin/UpdateProduct";
 import Search from "./pages/Search";
 import ProductDetails from "./pages/ProductDetails";
+import Categories from "./pages/Categories";
+import CategoryProduct from "./pages/CategoryProduct";
 
 function App() {
   return (
@@ -27,6 +29,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:slug" element={<CategoryProduct />} />
         <Route path="/search" element={<Search />} />
 
         <Route path="/dashboard" element={<PrivateRoute />}>
