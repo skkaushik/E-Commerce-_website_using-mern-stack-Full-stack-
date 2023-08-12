f81023c3204a4da88ff63a8cce140cecb7917c71
sachin <sksachinkaushik5362@gmail.com>
2023-08-12 10:25:51 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,54 @@
-
-import { Route,  Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/HomePage';
-import About from "./pages/About"
-import Contact from './pages/Contact';
-import Policy from './pages/Policy';
-import Pagenotfound from './pages/Pagenotfound';
-import Register from './pages/auth/Register';
-import Login from './pages/auth/Login';
-import Dashboard from './pages/user/Dashboard';
-import PrivateRoute from './components/Routes/Private';
-import ForgotPasswrd from './pages/auth/ForgotPasswrd';
-import AdminRoute from './components/Routes/AdminRoute';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import CreateCategory from './pages/admin/CreateCategory';
-import CreateProduct from './pages/admin/CreateProduct';
-import Orders from './pages/user/Orders';
-import Profile from './pages/user/Profile';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
+import ForgotPasswrd from "./pages/auth/ForgotPasswrd";
+import AdminRoute from "./components/Routes/AdminRoute";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import CreateCategory from "./pages/admin/CreateCategory";
+import CreateProduct from "./pages/admin/CreateProduct";
+import Orders from "./pages/user/Orders";
+import Profile from "./pages/user/Profile";
+import Products from "./pages/admin/Products";
+import UpdateProduct from "./pages/admin/UpdateProduct";
 
 function App() {
   return (
-   <>
-  
-  <Routes>
-    <Route path='/' element={<HomePage/>}/>
-    <Route path='/dashboard' element={<PrivateRoute/>}>
-    <Route path='user' element={<Dashboard/>}/>
-    <Route path="user/orders" element={<Orders />} />
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
           <Route path="user/profile" element={<Profile />} />
+        </Route>
 
-    </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
 
-    <Route path='/dashboard' element={<AdminRoute/>}>
-    <Route path='admin' element={<AdminDashboard/>}/>
-    <Route path="admin/create-category" element={<CreateCategory />} />
-          <Route path="admin/create-product" element={<CreateProduct/>} />
+          <Route path="admin/products" element={<Products/>} />
 
-    </Route>
-   
-    <Route path='/register' element={<Register/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/forgot-password' element={<ForgotPasswrd/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/contact' element={<Contact/>}/>
-    <Route path='/policy' element={<Policy/>}/>
-    <Route path='/*' element={<Pagenotfound/>}/>
+        </Route>
 
-
-  </Routes>
-
-   
-    
-   </>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPasswrd />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<Pagenotfound />} />
+      </Routes>
+    </>
   );
 }
 
